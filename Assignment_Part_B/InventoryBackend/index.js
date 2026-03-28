ba4f0271a52d9d64b4f106a0d127f2b28fae4b53
ANKITs-Projects <ankitgupta0429@gmail.com>
2026-03-28 13:45:08 +0530

    Vercel setup

--- Assignment_Part_B/InventoryBackend/index.js
+++ Assignment_Part_B/InventoryBackend/index.js
@@ -11,6 +11,10 @@ app.use(express.json())
 app.use("/supplier", require("./routes/supplierRoutes"))
 app.use("/inventory", require("./routes/inventoryRoutes"))
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000")
-})
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+// app.listen(5000, () => {
+//   console.log("Server running on port 5000")
+// })
